a91dddbaf2165a66a9daeff382af741b1cf54662
Ricardo de Azambuja <ricardo.azambuja@gmail.com>
2019-11-19 13:55:40 -0500

    now it outputs distance in mm

--- examples/motion.py
+++ examples/motion.py
@@ -20,24 +20,44 @@ args = parser.parse_args()
 flo = PMW3901(spi_port=0, spi_cs=1, spi_cs_gpio=BG_CS_FRONT_BCM if args.spi_slot == 'front' else BG_CS_BACK_BCM, secret_sauce=3)
 flo.set_rotation(args.rotation)
 
-tx = 0
-ty = 0
+# https://forum.bitcraze.io/viewtopic.php?t=2876
+# http://lup.lub.lu.se/luur/download?func=downloadFile&recordOId=8905295&fileOId=8905299
+NofPixels = 30.0
+Apperture = (4.2*3.14/180.0) # in degrees
+
+LP_CONSTANT = 0.8
+
+h = 20 # distance to the surface, in mm
 
 last_sample = time.time()
-
+x = 0
+y = 0
+prev_dx = 0
+prev_dy = 0
 try:
     while True:
         try:
-            x, y, q = flo.get_motion_with_quality()
+            dx, dy, q = flo.get_motion_with_quality()
         except RuntimeError:
             continue
-        tx += x
-        ty += y
-        print("Relative: x {:03d} y {:03d} | Absolute: x {:03d} y {:03d} | Quality: {}".format(x,y,tx,ty,q))
+
         dt = time.time()-last_sample
+
+        dx = LP_CONSTANT * prev_dx + (1.0 - LP_CONSTANT) * dx
+        prev_dx = dx
+
+        dy = LP_CONSTANT * prev_dy + (1.0 - LP_CONSTANT) * dy
+        prev_dy = dy
+
+        vx = h*dx*Apperture/(dt*NofPixels)
+        vy = h*dy*Apperture/(dt*NofPixels)
+        x += dt*vx
+        y += dt*vy
+        print("Sensor: dx {:03f} dy {:03f} | Distance (mm): x {:03f} y {:03f} | Quality: {}".format(dx,dy,x,y,q))
+
         if dt < flo.SAMPLE_INTERVAL:
             time.sleep(flo.SAMPLE_INTERVAL-dt)
 
         last_sample = time.time()
-except KeyboardInterrupt:
-    pass
+finally:
+    flo.reset()
